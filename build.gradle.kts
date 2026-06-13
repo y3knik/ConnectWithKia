c3531d9f846b636bd3fd7d5c70e090317672bb84
Nikhil Bhatia <github@bhatian.com>
2026-06-12 20:25:58 -0400

    chore: add gradle wrapper, version catalog, root build

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint) apply true
+    alias(libs.plugins.detekt) apply true
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
+}
